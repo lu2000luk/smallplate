2b9183cf0b1b283da36f220cba2e75adb117269f
lu2000luk <81090607+lu2000luk@users.noreply.github.com>
2026-03-07 18:32:04 +0100

    tuff

--- plate-manager/index.ts
+++ plate-manager/index.ts
@@ -7,7 +7,7 @@ import { mkdir } from "node:fs/promises";
 await mkdir("data", { recursive: true });
 export const db = new Database("data/plate.db", { create: true });
 
-console.log("[plate::manager] Version " + pkg.version);
+log("Version " + pkg.version);
 
 log("Initializing database...");
 db.run("PRAGMA journal_mode = WAL;");
@@ -17,10 +17,26 @@ log("Database initialized successfully.");
 log("Serving web server...");
 
 const server = Bun.serve({
+  port: process.env.PORT ? parseInt(process.env.PORT) : 3200,
   fetch(req) {
     const url = new URL(req.url);
+    log("INBOUND", req.method, url.pathname);
+    if (url.pathname === "/") {
+      const now = new Date();
+      const timestamp = `${String(now.getHours()).padStart(2, "0")}:${String(now.getMinutes()).padStart(2, "0")}:${String(now.getSeconds()).padStart(2, "0")}.${String(now.getMilliseconds()).padStart(3, "0")}`;
 
-    return new Response("404 Not Found", { status: 404 });
+      return new Response(
+        "Plate Manager v" + pkg.version + " (" + timestamp + ")",
+        {
+          status: 200,
+        },
+      );
+    }
+
+    if (url.pathname.startsWith("/auth")) {
+    }
+
+    return new Response("[404] Not Found", { status: 404 });
   },
 });
 
--- plate-manager/log.ts
+++ plate-manager/log.ts
@@ -1,13 +1,42 @@
+function getTimestampAndFunctionName(): string {
+  const now = new Date();
+  const timestamp = `${String(now.getHours()).padStart(2, "0")}:${String(now.getMinutes()).padStart(2, "0")}:${String(now.getSeconds()).padStart(2, "0")}.${String(now.getMilliseconds()).padStart(3, "0")}`;
+  const stack = new Error().stack;
+  if (stack) {
+    const lines = stack.split("\n");
+    if (true) {
+      // @ts-ignore
+      const callerLine = lines[3].trim().split("\\").at(-1) || undefined;
+      if (callerLine) {
+        return `\x1b[90m${timestamp} <\x1b[0;38;2;104;173;0;49m${callerLine
+          .replaceAll(")", "")
+          .replaceAll("(", "")}\x1b[90m>\x1b[0m`;
+      }
+    }
+  }
+  return `[${timestamp}]`;
+}
+
 export function log(...args: any[]) {
-  console.log("[plate::manager]", ...args);
+  console.log(
+    "\x1b[34m[plate::manager]\x1b[0m",
+    getTimestampAndFunctionName(),
+    ...args,
+  );
 }
 
 export function error(...args: any[]) {
-  console.error("[plate::manager] Error: ", ...args);
+  console.error(
+    "\x1b[31m[plate::manager]\x1b[0m " + getTimestampAndFunctionName(),
+    ...args,
+  );
 }
 
 export function warn(...args: any[]) {
-  console.warn("[plate::manager] Warning: ", ...args);
+  console.warn(
+    "\x1b[33m[plate::manager]\x1b[0m " + getTimestampAndFunctionName(),
+    ...args,
+  );
 }
 
 export function tryCatch<T>(fn: () => T, errorMessage: string): T | null {
